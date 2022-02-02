d2a30f8471b73a13bb489012e82ed221936b375b
AirHua-byte <3301833942@qq.com>
2022-02-02 17:22:43 +0800

    alpha1.0

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 // import mUI from './components'
-// import mUI from '../lib/air-element-components.es'
-// import '../lib/style.css'
-import chooseIcon from '../lib/chooseIcon/index.es'
-import '../lib/chooseIcon/style.css'
+import mUI from '../lib/index.es'
+import '../lib/style.css'
 import './mock'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
-app.use(chooseIcon)
+app.use(mUI)
 app.mount('#app')
